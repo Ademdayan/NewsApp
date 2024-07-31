5d0c700a9170848a2a693f09c2077bec94c90915
Ademdayan <210912018@alparslan.edu.tr>
2024-07-31 08:53:53 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/pages/news_page.dart';
+import 'package:news_app/viewmodel/article_list_view_model.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Maykod Haberler',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home:
+      ChangeNotifierProvider(
+        create: (context) => ArticleListViewModel(),
+        child: const NewsPage(),
+
+      ),
+    );
+  }
+}
